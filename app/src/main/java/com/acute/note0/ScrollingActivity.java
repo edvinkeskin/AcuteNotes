4c953cedcf7896fe82848f4831aebaf986b713e7
ekeskin <ekeskin@students.cs.ubc.ca>
2021-06-19 14:39:51 +0300

    editable scrolling single-page notepad

--- app/src/main/java/com/acute/note0/ScrollingActivity.java
+++ app/src/main/java/com/acute/note0/ScrollingActivity.java
@@ -20,6 +20,14 @@ public class ScrollingActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
