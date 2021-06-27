eeb07c7ca863b500586bc3723e5c7f1347e6728c
ekeskin <ekeskin@students.cs.ubc.ca>
2021-06-27 15:58:59 +0300

    persistance via stream tried

--- app/src/main/java/com/acute/note0/ScrollingActivity.java
+++ app/src/main/java/com/acute/note0/ScrollingActivity.java
@@ -1,6 +1,6 @@
 package com.acute.note0;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.EditText;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -9,10 +9,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class ScrollingActivity extends AppCompatActivity {
     private static int notes = 1;
     private static String id;
     private static Note note;
+    //note status true if cached, false if it has to be fetched
+    private static HashMap<String,Boolean> noteStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,9 @@ public class ScrollingActivity extends AppCompatActivity {
         note = new Note(id);
         toolBarLayout.setTitle(id);
         toolbar.setTitle(id);
+
+        noteStatus = new HashMap<>();
+        String notepad[] = readNotepad();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> nextNote());
@@ -41,13 +50,22 @@ public class ScrollingActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
     private void prevNote() {
         EditText input = findViewById(R.id.input);
         note.setNote(input.getText().toString());
+        writeNote(id);
         Toolbar toolbar = findViewById(R.id.toolbar);
         note = note.getPreviousNote();
-        input.setText(note.getNote());
         id = note.getId();
+        readNote(id);
+
+        input.setText(note.getNote());
         toolbar.setTitle(id);
     }
 
@@ -59,10 +77,55 @@ public class ScrollingActivity extends AppCompatActivity {
             id = "Note" + notes++;
             note = new Note(id,note);
         } else {
+            writeNote(id);
             note = note.getNextNote();
             id = note.getId();
+            readNote(id);
         }
+
         input.setText(note.getNote());
         toolbar.setTitle(id);
+    }
+
+    private void writeNote(String filename) {
+        //WRITE
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(note.getNote().getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void readNote(String filename) {
+        //READ
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(filename);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Scanner scanner = new Scanner(fis);
+        scanner.useDelimiter("\\Z");
+        String content = scanner.next();
+        scanner.close();
+    }
+
+    private String[] readNotepad() {
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput("notepad");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Scanner scanner = new Scanner(fis);
+        scanner.useDelimiter("\\Z");
+        String content = scanner.next();
+        scanner.close();
     }
 }
